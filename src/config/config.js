808dfc0324841ab171b078f3c420c95d48a9c65a
EC2 Default User <ec2-user@ip-172-31-22-185.ec2.internal>
2022-06-24 14:09:36 +0000

    mis à jour du serveur$

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //SERVER:"https://back.lovons.com:4000",
-    SERVER:"http://localhost:4000",
+    SERVER:"https://back.lovons.com:4000",
+    //SERVER:"http://localhost:4000",
    rezise :"https://res.cloudinary.com/dzjjthglw/image/fetch/h_400/",
    rezisecarrousel :"https://res.cloudinary.com/dzjjthglw/image/fetch/h_520/",
    defaultlovonsimage:"https://res.cloudinary.com/serpoma/image/upload/v1651135782/lovons-coeur.png",
