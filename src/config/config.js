dc3ed76a4306b5e5cc2e0aad94b96fc65a891bb2
tabue romeo <tabueromeo@gmail.com>
2023-02-09 15:42:41 +0100

    ajout de gcp

--- src/config/config.js
+++ src/config/config.js
@@ -1,34 +1,34 @@
 module.exports = {
   //SERVER:"https://back.lovons.com:4000",
-  SERVER:"http://localhost:4000",
-   rezise :"https://res.cloudinary.com/dzjjthglw/image/fetch/h_400/",
-   rezisecarrousel :"https://res.cloudinary.com/dzjjthglw/image/fetch/h_520/",
-   defaultlovonsimage:"https://res.cloudinary.com/serpoma/image/upload/v1651135782/lovons-coeur.png",
-   whatsappapi:"https://api.whatsapp.com/send/?phone=+237",
-   wareframeParagraph:"https://react.semantic-ui.com/images/wireframe/paragraph.png",
-   title:{
-    thot:"Rencontre hot",
-    tesc:"Escorte",
-    tsoir:"Juste pour un soir",
-    tmas:"Massage",
-    tproduit:"Produit adulte",
-    },
-   ville:{
-       douala:"Douala",
-       yaounde:"Yaoundé"
-   },
-   cherche:{
-       homme:"Homme",
-       femme:"Femme",
-       couple:"Couple",
-   },
-   statut:{
-    ligne:"En ligne",
-    attente:"En attente",
-    supprime:"Supprimée"
-   },
-   inputTextTitlemaxLength:100,
-   inputTextDescriptionmaxLength:1000,
-   
-    
-}
+  SERVER: 'https://civic-matrix-377214.oa.r.appspot.com',
+  rezise: 'https://res.cloudinary.com/dzjjthglw/image/fetch/h_400/',
+  rezisecarrousel: 'https://res.cloudinary.com/dzjjthglw/image/fetch/h_520/',
+  defaultlovonsimage:
+    'https://res.cloudinary.com/serpoma/image/upload/v1651135782/lovons-coeur.png',
+  whatsappapi: 'https://api.whatsapp.com/send/?phone=+237',
+  wareframeParagraph:
+    'https://react.semantic-ui.com/images/wireframe/paragraph.png',
+  title: {
+    thot: 'Rencontre hot',
+    tesc: 'Escorte',
+    tsoir: 'Juste pour un soir',
+    tmas: 'Massage',
+    tproduit: 'Produit adulte',
+  },
+  ville: {
+    douala: 'Douala',
+    yaounde: 'Yaoundé',
+  },
+  cherche: {
+    homme: 'Homme',
+    femme: 'Femme',
+    couple: 'Couple',
+  },
+  statut: {
+    ligne: 'En ligne',
+    attente: 'En attente',
+    supprime: 'Supprimée',
+  },
+  inputTextTitlemaxLength: 100,
+  inputTextDescriptionmaxLength: 1000,
+};
